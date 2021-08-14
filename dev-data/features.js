1a051ff8eaa8dfab64bb51231f1ad6229e38ff15
helkiayeremia <helkia77@gmail.com>
2021-08-14 20:49:13 +0700

    kecamatan done

--- dev-data/features.js
+++ dev-data/features.js
@@ -10,6 +10,149 @@ const features = {
         type: 'Point',
         coordinates: [-7.820563613465092, 110.36128423972247, 0]
       }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '1',
+        mag: 3
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.8056751470535, 110.36280298204916, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '2',
+        mag: 4
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.8175390119261765, 110.37333412623236, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '3',
+        mag: 2
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.809683875118657, 110.38758809024138, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '4',
+        mag: 4
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.817877462574211, 110.39533788376012, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '5',
+        mag: 5
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.786886383876819, 110.38740114400196, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '6',
+        mag: 3
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.794142676237758, 110.37299808171397, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '7',
+        mag: 3
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.799541916104912, 110.37784102481777, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '8',
+        mag: 2
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.803008949034309, 110.36603126201813, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '9',
+        mag: 4
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.806165381954313, 110.35593125023487, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '10',
+        mag: 4
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.8107002941700925, 110.3480162278344, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '11',
+        mag: 6
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.789254059593952, 110.35886920243351, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '12',
+        mag: 1
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.783175241665449, 110.36233244194858, 0]
+      }
+    },
+    {
+      type: 'Feature',
+      properties: {
+        id: '13',
+        mag: 5
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-7.782970363494183, 110.3557126697926, 0]
+      }
     }
   ]
 };
